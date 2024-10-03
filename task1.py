e16e90434531f27afe7f06f1fc1ef597f6b971b8
Rodulfo-Gwapoe <tagnaaba@mail.com>
2024-10-03 11:43:08 +0800

    loop task

---
+++ task1.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Number : "))
+FavFruits = []
+for data in range(num):
+    print(FavFruits)
+    fruits = input("Enter Fruits : ")
+    FavFruits.append(fruits)  
+print(FavFruits)
---
+++ task2.py
@@ -0,0 +1,9 @@
+Fruits = ["banana", "apple", "orange", "mango"]
+for data in Fruits:
+    if data == "banana":
+        print("hellow banana")
+    elif data == "apple":
+        print("hellow apple")
+        break
+    else:
+        print("no")
